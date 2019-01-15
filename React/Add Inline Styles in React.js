f21562aa3633ca2c8bf438a50632d5c879fe33bd
devCamper <44700391+devCamper@users.noreply.github.com>
2019-01-15 08:57:38 +0100

    Create

---
+++ React/Add Inline Styles in React.js
@@ -0,0 +1,13 @@
+
+// const styles =
+// change code above this line
+class Colorful extends React.Component {
+    render() {
+      // change code below this line
+      return (
+        <div style={{color: "yellow", fontSize: 24}}>Style Me!</div>
+      );
+      // change code above this line
+    }
+  };
+  
